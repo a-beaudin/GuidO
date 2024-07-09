100f07b9d13ed053e8b5f4dba7c83e77fde11e02
Amélie Beaudin <117298830+a-beaudin@users.noreply.github.com>
2024-07-09 17:35:40 -0400

    Create GuidOApp.swift

---
+++ GuidOApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuidOApp.swift
+//  GuidO
+//
+//  Created by Amelie Beaudin on 2024-06-14.
+//
+
+import SwiftUI
+
+@main
+struct GuidOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
